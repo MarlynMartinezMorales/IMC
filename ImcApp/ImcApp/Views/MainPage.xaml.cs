dda640a691453f6a78cbd849305a9e1b2e131494
Martinez Morales Marlyn Gpe <17470313@campeche.tecnm.mx>
2022-01-11 23:09:16 -0600

    Se agrego el patrón de diseño MVVM(Model-View-ViewModel)

---
+++ ImcApp/ImcApp/Views/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ImcApp.Models;
+using System;
+using Xamarin.Forms;
+using ImcApp.ViewModels;
+
+
+namespace ImcApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            BindingContext = viewModel;
+            //LimpiarIU();
+        }
+
+        private string GetEstadoNutricional(CalculadoraImc.EstadoNutricional estado)
+        {
+            switch (estado)
+            {
+                case CalculadoraImc.EstadoNutricional.PesoBajo:
+                    return Resources["TextoPesoBajo"] as string;
+                case CalculadoraImc.EstadoNutricional.PesoNormal:
+                    return Resources["TextoPesoNormal"] as string;
+                case CalculadoraImc.EstadoNutricional.SobrePeso:
+                    return Resources["TextoSobrepeso"] as string;
+                case CalculadoraImc.EstadoNutricional.Obesidad:
+                    return Resources["TextoObesidad"] as string;
+                case CalculadoraImc.EstadoNutricional.ObesidadExtrema:
+                    return Resources["TextoObesidadExtrema"] as string;
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
